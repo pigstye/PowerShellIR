f50ce9d0c32b9d59f53b236b5d67ca1d5dee625d
Tom Willett <tomw@pigstye.net>
2015-09-11 12:05:11 -0400

    Initial Commit

---
+++ convert-xor.ps1
@@ -0,0 +1,48 @@
+<#
+
+.SYNOPSIS
+
+Xor two strings
+
+.DESCRIPTION
+
+Xor two strings
+
+.PARAMETER $InString
+
+String to convert (required)
+
+.PARAMETER $XorKey
+
+String to xor with
+
+.EXAMPLE     
+    .\convert-xor.ps1 "asdfasdf" "sdf"
+    Xors "asdfasdf" "sdf"  the second string is repeated as needed
+
+.NOTES
+	
+ Author: Tom Willett
+ Date: 11/13/2014
+ © 2014 Oink Software
+
+#>
+
+Param([Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][string[]]$InString, [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][string[]]$XorKey)
+
+begin {
+	$NewString = ""
+}
+process {
+	$xorLength = $XorKey.length
+	$ctr = 0
+	Foreach($Char in $InString.ToCharArray())
+	{
+		$NewString += $char -bxor $XorKey[$ctr]
+		$ctr++
+		if ($ctr -eq $xorLength) { $ctr = 0 }
+	} 
+}
+end {
+	$newString
+}

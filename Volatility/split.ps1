92022032f8be3bc31438ac5722d947f4d97aebf9
Tom Willett <tomw@pigstye.net>
2015-09-20 12:50:34 -0400

    Initial Commit

---
+++ Volatility/split.ps1
@@ -0,0 +1,43 @@
+<#
+
+.SYNOPSIS
+
+Split a file into smaller sized files
+
+.DESCRIPTION
+
+Split a file. This uses the .net file routines.  By default it splits it into 200mb chuncks.  You can change the size by altering the $bufSize variable.
+The parts are named by adding 1 2 3 etc to the file name.
+
+.PARAMETER inFile
+
+The file to split (required)
+
+.EXAMPLE     
+    .\split.ps1 c:\image.mem
+    Splits c:\image.mem into 200MB chuncks c:\image1.mem, c:\image2.mem, c:\image3.mem
+
+.NOTES
+	
+ Author: Tom Willett
+ Date: 12/15/2014
+ © 2013 Oink Software
+
+#>
+
+Param([Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][string]$inFile)
+$bufSize = 200000000
+$stream = [System.IO.File]::OpenRead($inFile)
+$chunkNum = 1
+$barr = New-Object byte[] $bufSize
+$basename = $inFile.substring(0,$inFile.lastindexof("."))
+$ext = $inFile.substring($inFile.lastindexof("."))
+while( $bytesRead = $stream.Read($barr,0,$bufsize)){
+	$outFile = "$basename$chunkNum$ext"
+	$ostream = [System.IO.File]::OpenWrite($outFile)
+	$ostream.Write($barr,0,$bytesRead);
+	$ostream.close();
+	write-host "wrote $outFile"
+	$chunkNum += 1
+}
+
---
+++ test-wrm.ps1
@@ -0,0 +1,72 @@
+<#
+
+.SYNOPSIS     
+    Tests whether a computer has WinRM running.   
+     
+.DESCRIPTION   
+    Tests whether a computer either local or remote has WinRM running.    
+	It returns a powershell object with the computername and whether 
+	or not WinRm is running.
+      
+.PARAMETER computername   
+    Name of computer to test.
+       
+.EXAMPLE     
+    .\Test-wrm.ps1
+    Checks whether WinRM is running on local computer
+
+.EXAMPLE     
+    .\Test-wrm.ps1 -computername 'servername'
+    Checks whether WinRM is running on computer 'servername'
+
+.EXAMPLE     
+    type computers.txt | .\Test-wrm.ps1 | export-csv -notypeinformation winrm.csv
+    Checks whether WinRM is running on the computers listed (one per line) in the file
+	computers.txt and outputs the results to a csv file called winrm.csv
+
+	.NOTES     
+	Author: Tom Willett
+	Date: 8/28/2014
+	© 2014 Oink Software
+
+#>
+
+Param([Parameter(Mandatory=$False,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][string]$computername = "")
+
+begin {
+	$report = @()
+}
+
+process {
+	$error.clear()
+	if ($computername -eq "") {
+		$wrm = test-wsman -EA SilentlyContinue
+		$computername = $env:computername
+		$pingable = $True
+	} else {
+		$Pingable = ""
+		$temp = $null
+		$ping = New-Object System.Net.NetworkInformation.Ping
+		$temp = $ping.send($computername,3000)
+		$Pingable= $temp.Status.toString()
+		if ($Pingable -eq "Success") {
+			$wrm = test-wsman $computername -EA SilentlyContinue
+			$Pingable = $True
+		} else {
+			$Pingable = $False
+		}
+	}
+	$tmp = "" | Select Computername, Pingable, WSMANEnabled
+	$tmp.Computername = $computername
+	$tmp.Pingable = $Pingable
+	if ($error.count -gt 0) {
+		$tmp.WSMANEnabled = "False"
+	} else {
+		$tmp.WSMANEnabled = "True"
+	}
+	$report += $tmp
+}
+
+end {
+	$report
+}
